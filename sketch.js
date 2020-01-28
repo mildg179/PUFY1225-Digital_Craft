f888a2042eed341136b2cfcc69a3224c53fb85e1
mildg179 <60146926+mildg179@users.noreply.github.com>
2020-01-28 11:30:43 -0500

    Mondrian

---
+++ sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("white");
+  strokeWeight(5);
+  fill("white");
+  rect(0, 0, 75, 50);
+  fill("white");
+  rect(75, 0, 100, 50);
+  fill("yellow");
+  rect(150, 0, 100, 50);
+  fill("yellow");
+  rect(150, 50, 100, 100);
+  fill("white");
+  rect(-50, 50, 75, 100);
+  fill("white");
+  rect(-50, 150, 75, 150);
+  fill("yellow");
+  rect(-50, 300, 75, 150);
+  fill("red");
+  rect(25, 50, 125, 200);
+  fill("black");
+  rect(25, 250, 60, 100);
+  fill("white");
+  rect(90, 250, 60, 50);
+  fill("white");
+  rect(90, 300, 60, 50);
+  fill("black");
+  rect(90, 350, 60, 50);
+  fill("blue");
+  rect(150, 300, 100, 100);
+  fill("white");
+  rect(150, 150, 50, 100);
+  fill("white");
+  rect(200, 150, 50, 100);
+  fill("red");
+  rect(250, 300, 100, 100);
+  fill("white");
+  rect(250, 0, 100, 300);
+}
