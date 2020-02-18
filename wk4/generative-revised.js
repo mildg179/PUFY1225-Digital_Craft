fa257dbf7a211d9545b44e1dc70af47d3a5f4926
mildg179 <60146926+mildg179@users.noreply.github.com>
2020-02-17 22:17:07 -0500

    Add files via upload

---
+++ wk4/generative-revised.js
@@ -0,0 +1,28 @@
+xRad = 15;
+yRad = 90;
+
+var odds;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+fill('Red');
+textSize(45);
+textFont('Helvetica');
+text('CODE', 130, 395);
+
+  
+for(var x= xRad/2; x< width; x+=xRad){
+    for(var y= yRad/2; y < height; y+=yRad){
+      fill("red");
+      ellipse(x, y, xRad, yRad);
+      strokeWeight(5);
+      line(200,200,x,y)
+      
+    }
+  }
+}
