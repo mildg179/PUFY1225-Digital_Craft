fcf9330dd81152fb4ff36f1c0f6484fa29582f9c
mildg179 <60146926+mildg179@users.noreply.github.com>
2020-03-03 09:14:12 -0500

    Update countdown.js

--- wk6/countdown.js
+++ wk6/countdown.js
@@ -10,15 +10,8 @@ function draw() {
   textSize(100);
   text(timer, width/2, height/2);
   
-  // while (timer > 0) {  // this doesn't work because it's all happening at the same time
-  //   timer --;
-  // }
   
-  // frameCount --> this keeps track of the number of times the program has gone throught the code, 60 = 1 second
-  // % ---> this is the Modulo operator, it divides numbers and evaluates to the remainder: 17 % 5 evaluates to 2 remainder
-  // this can be used to determine if the number on the left is divisible by the number on the right
-  
-  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+  if (frameCount % 60 == 0 && timer > 0) { 
     timer --;
   }
   if (timer == 0) {
